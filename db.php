bae076275d534fce6900ea5d96f47bb5c4e4eaba
JuniorTecsup <54334297+JuniorTecsup@users.noreply.github.com>
2020-03-26 12:30:41 -0500

    v1

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("ocvwlym0zv3tcn68.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","hba90vutj8x99kac","np0qawe93yjdcgld","b83b60bwal8v2w60");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
